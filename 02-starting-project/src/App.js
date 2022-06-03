1f88b870ddc580bf39e0c11c604c2596629b4c3e
Jana <2048710@cognizant.com>
2022-06-03 12:31:37 +0530

    02 file added commit

---
+++ 02-starting-project/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserhandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { id: Math.random().toString(), name: uName, age: uAge },
+      ];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserhandler}></AddUser>
+      <UsersList users={usersList}></UsersList>
+    </div>
+  );
+}
+
+export default App;
