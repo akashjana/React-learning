c2ac852364213e12577ddff6db498ad20f047fa3
Jana <2048710@cognizant.com>
2022-06-09 12:48:54 +0530

    04 food delivry app order step completed

--- 04-Food delivery-app/src/components/Cart/Cart.js
+++ 04-Food delivery-app/src/components/Cart/Cart.js
@@ -1,10 +1,16 @@
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
+import { Fragment } from "react";
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isConfirming, setIsConfirming] = useState(false);
+  const [didConfirm, setDidConfirm] = useState(false);
+
   const cartCtx = useContext(CartContext);
   const totalAmount = `Rs.${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
@@ -15,6 +21,28 @@ const Cart = (props) => {
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({ ...item, amount: 1 });
   };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setIsConfirming(true);
+    await fetch(
+      "https://react-http-demo-dc005-default-rtdb.firebaseio.com/orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+    setIsConfirming(false);
+    setDidConfirm(true);
+    cartCtx.clearCart();
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
@@ -29,19 +57,54 @@ const Cart = (props) => {
       ))}
     </ul>
   );
-  return (
-    <Modal onClose={props.onClose}>
+
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  const cartModalContent = (
+    <Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
+      {isCheckout && (
+        <Checkout
+          onConfirm={submitOrderHandler}
+          onCancel={props.onClose}
+        ></Checkout>
+      )}
+      {!isCheckout && modalActions};
+    </Fragment>
+  );
+
+  const isConfirmingModalContent = <p>Sending order data...</p>;
+  const didConfirmedModalContent = (
+    <Fragment>
+      <p>Order placed successfully!</p>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>
+        <button className={classes.button} onClick={props.onClose}>
           Close
         </button>
-        {hasItems && <button className={classes.button}>Order</button>}
       </div>
+    </Fragment>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isConfirming && !didConfirm && cartModalContent}
+      {isConfirming && isConfirmingModalContent}
+      {!isConfirming && didConfirm && didConfirmedModalContent}
     </Modal>
   );
 };
