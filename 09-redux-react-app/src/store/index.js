a8613e0ac47c2793983c2a843a65057e87710fda
Jana <2048710@cognizant.com>
2022-06-10 10:48:19 +0530

    09 redux basic completed

--- 09-redux-react-app/src/store/index.js
+++ 09-redux-react-app/src/store/index.js
@@ -1,26 +1,6 @@
-import { createStore } from "redux";
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const initialState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counter";
+import authSlice from "./auth";
 
 // const counterReducer = (state = initialState, action) => {
 //   if (action.type === "increment") {
@@ -52,8 +32,10 @@ const counterSlice = createSlice({
 // const store = createStore(counterReducer);
 
 const store = configureStore({
-    reducer:counterSlice.reducer
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
 
-export const counterActions = counterSlice.actions;
 export default store;
