e52ac927a7e5dabdd88486b883c131303440532c
Jana <2048710@cognizant.com>
2022-06-10 12:24:15 +0530

    09 project setup completed - redux added

--- 10-advanced-redux/src/components/Shop/Products.js
+++ 10-advanced-redux/src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 50,
+    title: "The Book",
+    description: "The best book i ever read.",
+  },
+  {
+    id: "p2",
+    price: 10,
+    title: "The Book 2",
+    description: "The 2nd best book i ever read.",
+  },
+  {
+    id: "p3",
+    price: 30,
+    title: "The Book 3",
+    description: "The 3rd best book i ever read.",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
