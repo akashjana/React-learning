173093ca401ad14327e9764b89bc1af8b2e4511f
Jana <2048710@cognizant.com>
2022-06-13 12:09:39 +0530

    11 react router completed

--- 11-react router/src/App.js
+++ 11-react router/src/App.js
@@ -3,6 +3,9 @@ import { Fragment } from "react";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+import { Switch } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 function App() {
   return (
@@ -10,12 +13,20 @@ function App() {
       <Fragment>
         <MainHeader></MainHeader>
         <main>
-          <Route path="/welcome">
-            <Welcome></Welcome>
-          </Route>
-          <Route path="/products">
-            <Products></Products>
-          </Route>
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/welcome"></Redirect>
+            </Route>
+            <Route path="/welcome">
+              <Welcome></Welcome>
+            </Route>
+            <Route path="/products" exact>
+              <Products></Products>
+            </Route>
+            <Route path="/products/:productId">
+              <ProductDetail></ProductDetail>
+            </Route>
+          </Switch>
           <h3>Home Page!</h3>
         </main>
       </Fragment>
